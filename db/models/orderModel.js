7565f421e6fa3aa12c14bbea67ea9b1529ac1b3c
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-10-21 22:06:08 +0300

    fix order model

--- db/models/orderModel.js
+++ db/models/orderModel.js
@@ -2,15 +2,28 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    Articul: { type: String, required: true },
-    RetailPrice: { type: Number, required: true },
-    RetailPriceWithDiscount: { type: Number },
-    ModelName: { type: String, required: true },
     name: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true },
+
+    extraFields: { type: mongoose.Schema.Types.Mixed, required: false },
+
+    cartItems: [
+      {
+        Articul: { type: String, required: true },
+        RetailPrice: { type: Number, required: true },
+        RetailPriceWithDiscount: { type: Number },
+        ModelName: { type: String, required: true },
+        quantity: { type: Number, required: true },
+
+        additionalFields: {
+          type: mongoose.Schema.Types.Mixed,
+          required: false,
+        },
+      },
+    ],
   },
-  { timestamps: true }
+  { timestamps: true, strict: false }
 );
 
 const Order = mongoose.model("Order", orderSchema);
